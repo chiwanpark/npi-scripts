88c25e5ded74ff49d5dc791c1b87821a4bdf140a
Chiwan Park <chiwanpark@hotmail.com>
2020-07-18 19:59:39 +0900

    Update rtorrent image

--- image/rtorrent/config.js
+++ image/rtorrent/config.js
@@ -1,14 +1,17 @@
 const CONFIG = {
   baseURI: process.env.FLOOD_BASE_URI || '/',
   dbCleanInterval: 1000 * 60 * 60,
-  dbPath: '/home/rtorrent/flood/server/db/',
+  dbPath: '/home/rtorrent/flood.git/server/db/',
+  floodServerHost: '0.0.0.0',
   floodServerPort: 3000,
   maxHistoryStates: 30,
   pollInterval: 1000 * 5,
   secret: process.env.FLOOD_SECRET || 'flood',
   scgi: {
     socket: true,
-    socketPath: process.env.RTORRENT_SOCK_PATH || '/home/rtorrent/session/rtorrent.sock',
+    socketPath: process.env.RTORRENT_SOCK_PATH || '/home/rtorrent/session/rpc.sock',
   },
   ssl: false
 };
+
+module.exports = CONFIG;
